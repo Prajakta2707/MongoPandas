19dcc90dd1d0ad6777bdf9525f43ea339e72ea6d
Prajakta2707 <prajaktalatane03@gmail.com>
2025-06-02 13:10:52 +0530

    Initial commit

---
+++ app1.py
@@ -0,0 +1,57 @@
+
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from bson import ObjectId
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["MyDatabase"]
+collection = db["PractiseDB"]
+
+# Helper: convert Mongo ObjectId to string
+def serialize_doc(doc):
+    doc["_id"] = str(doc["_id"])
+    return doc
+
+# GET all documents
+@app.route("/data", methods=["GET"])
+def get_all():
+    data = [serialize_doc(doc) for doc in collection.find()]
+    return jsonify(data)
+
+# GET by ID
+@app.route("/data/<id>", methods=["GET"])
+def get_by_id(id):
+    doc = collection.find_one({"_id": ObjectId(id)})
+    if doc:
+        return jsonify(serialize_doc(doc))
+    return jsonify({"error": "Not found"}), 404
+
+# POST: Insert new document
+@app.route("/data", methods=["POST"])
+def create():
+    data = request.json
+    inserted = collection.insert_one(data)
+    return jsonify({"inserted_id": str(inserted.inserted_id)})
+
+# PUT: Update by ID
+@app.route("/data/<id>", methods=["PUT"])
+def update(id):
+    data = request.json
+    result = collection.update_one({"_id": ObjectId(id)}, {"$set": data})
+    if result.matched_count:
+        return jsonify({"updated": True})
+    return jsonify({"error": "Not found"}), 404
+
+# DELETE by ID
+@app.route("/data/<id>", methods=["DELETE"])
+def delete(id):
+    result = collection.delete_one({"_id": ObjectId(id)})
+    if result.deleted_count:
+        return jsonify({"deleted": True})
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- config.py
+++
@@ -1,2 +0,0 @@
-MONGO_URI = "mongodb+srv://<prajaktalatane03>:<MyMongo@1309>@cluster0.t81mpkh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DB_NAME = "bookdb"
